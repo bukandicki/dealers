e662df0261d16ca517ff6c62140850e5f0c39668
Dicki <bukandicki@gmail.com>
2022-09-03 20:58:58 +0700

    fix: Navbar responsive rules

--- src/components/Navbar/Navbar.component.vue
+++ src/components/Navbar/Navbar.component.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref } from "vue";
 import { navLinks } from "@/lib/constant";
 
 import Logo from "@/assets/icons/Logo.vue";
@@ -7,16 +8,23 @@ import ChevronDown from "@/assets/icons/ChevronDown.vue";
 import Euro from "@/assets/images/euro.png";
 
 import "./Navbar.modules.sass";
+
+const showNav = ref(false);
+
+const handleShowNav = () => {
+    showNav.value = !showNav.value;
+};
 </script>
 
 <template lang="pug">
 .navbar
     Logo.navbar__logo
-    ul.navbar__links
-        li
-            img.navbar__euro-link(:src="Euro")
-        li.navbar__link(v-for="link in navLinks" :key="link.id")
-            span {{ link.title }}
-            ChevronDown(v-if="link.hasDropdown")
-        li.navbar__hamburger(role="button")
+    .navbar__wrapper
+        ul.navbar__links(:class="{ 'navbar__links--show': showNav }")
+            li.navbar__link
+                img.navbar__euro-link(:src="Euro")
+            li.navbar__link(v-for="link in navLinks" :key="link.id")
+                span {{ link.title }}
+                ChevronDown(v-if="link.hasDropdown")
+        .navbar__hamburger(role="button" @click="handleShowNav")
 </template>
